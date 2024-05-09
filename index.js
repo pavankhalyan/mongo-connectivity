fd48a7d7d59525163bae0baa410603ac40499566
Pavan khalyan <pavankhalyan2004@gmail.com>
2024-05-09 22:34:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express ()
+app.use(express.json() )
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/").then(() => console.log("connected")).catch((err) => console.log(err))    
